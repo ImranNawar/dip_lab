ac6d720edf14b27a4ccfc80379253b2ae7128107
ImranNawar <imran1nawar@gmail.com>
2023-12-22 15:32:06 +0500

    Data Preprocessing

---
+++ 05_Machine-Learning/Part 1 - Data Preprocessing/data_preprocessing.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 22 09:32:38 2023
+
+@author: Imran Nawar
+"""
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer # Imputer class allow us to take care of those missing data
+imputer = SimpleImputer(strategy = 'mean')
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+   # Assuming the categorical feature is in the first column (index 0)
+column_transformer = ColumnTransformer(
+    transformers=[
+        ('onehot', OneHotEncoder(), [0])  # Apply OneHotEncoder to the first column
+    ],
+    remainder='passthrough'  # Keep the remaining columns as they are
+)
+X = column_transformer.fit_transform(X)
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
