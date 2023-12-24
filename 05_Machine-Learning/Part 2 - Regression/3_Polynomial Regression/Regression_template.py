d4631c72a19f71e1660dd49a2d264fea1ffd0327
ImranNawar <imran1nawar@gmail.com>
2023-12-24 08:53:39 +0500

    Regression Template

--- 05_Machine-Learning/Part 2 - Regression/3_Polynomial Regression/Regression_template.py
+++ 05_Machine-Learning/Part 2 - Regression/3_Polynomial Regression/Regression_template.py
@@ -18,39 +18,26 @@ y = dataset.iloc[:, 2].values
 """from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
-# Fitting Linear Regression to the dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+# Fitting the Regression Model to the dataset
+# Create your regressor here
 
-# Fitting Polynomial Regression to the dataset
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=4)
-X_poly = poly_reg.fit_transform(X)
-lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
+# Predicting a new result
+y_pred = regressor.predict(6.5)
 
-# Visualising the Linear Regression results
-plt.scatter(X, y, color='red')
-plt.plot(X, y, lin_reg.predict(X), color='blue')
-plt.title('Truth or Bluff (Linear Regression)')
+# Visualizing the Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualizing the Polynomial Regression results
+# Visualizing the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color='red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
+X_grid = X_grid.reshape(len(X_grid, 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-
-# Predicting a new result with Linear Regression
-lin_reg.predict(6.5)
-
-
-# Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
