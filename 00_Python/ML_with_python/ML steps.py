d1e0c7e46f9f6199ce199c782ac1e4a1e52f41d6
ImranNawar <imran1nawar@gmail.com>
2024-03-24 16:31:54 +0500

    A little

---
+++ 00_Python/ML_with_python/ML steps.py
@@ -0,0 +1,16 @@
+# Machine Learning with Python
+"""
+Created on Mon Mar 18 14:49:05 2024
+
+@author: Imran Nawar
+"""
+
+'''STEPS: 
+    Import the Data
+    Clean the Data
+    Split the Data into Training/Test set
+    Create a Model
+    Train the Model
+    Make Predictions
+    Evaluate and Improve
+'''
