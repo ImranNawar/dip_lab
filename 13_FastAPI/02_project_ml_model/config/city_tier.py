755adee602187b67ea4177b87428df293496bd6b
ImranNawar <imran1nawar@gmail.com>
2025-12-06 17:06:32 +0500

    Restructure the folder to Professional API

---
+++ 13_FastAPI/02_project_ml_model/config/city_tier.py
@@ -0,0 +1,3 @@
+tier_1_cities = ["Lahore", "Islamabad", "Peshawar", "Karachi"]
+tier_2_cities = ["Mardan", "Quetta", "Faisalabad","Rawalpindi","Multan","Sialkot","Gujranwala","Hyderabad","Sukkur","Bahawalpur",
+    "Sargodha","Abbottabad","Haripur","Swat","Kohat","Dera Ghazi Khan","Okara","Sheikhupura","Rahim Yar Khan","Chiniot"]
